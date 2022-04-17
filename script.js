3bbae426a9ff00f57563c5c1d874f021c3decba6
Gitbermudez <douglas_bermudez@hotmail.com>
2022-04-17 08:50:52 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 function compute()
 {
-    
     var principal = document.getElementById("principal").value;
+    
     /*(new Intl.NumberFormat('en-CA', {style:'currency', currency:'CAD', useGrouping: false}).format(principal));*/
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    
     var interest = principal * years * rate /100;  
-    
-    var year = new Date().getFullYear()+parseInt(years);     
+    var year = new Date().getFullYear()+parseInt(years);    
+    var result = document.getElementById("result"); 
     
    /* document.getElementById("result").innerHTML="If you deposit "+principal+",\<br/> at an interest rate of "+rate+"%\<br/> You will receive an amount of "+amount+",\<br/>in the year "+year+"\<br/>"*/
-      document.getElementById("result").innerHTML="If you deposit "+principal+" \<br\>at an interest rate of "+rate+"% \<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+   if (principal <="0")
+   {
+     //If all is well return true.
+     alert("Please enter a positive number")
+     document.getElementById("principal").focus();
+    /* return false;*/
+   }else{
+      document.getElementById("result").innerHTML="If you deposit..:\<mark\>"+principal+"\</mark\>\<br\>+at an interest rate of..:\<mark\>"+rate+"%\</mark\>\<br\>You will receive an amount of..\<mark\>"+interest+"\</mark\>\<br\>in the year..\<mark\> "+year+"\</mark\>\<br\>"
 
-   /*var interest = principal * year * rate /100;*/
-   /*p = document.getElementById("principal").value;*/
-    /*document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"*/
+   }
 }
+ /*else {
+        result.innerHTML = '\<br\>' + 'If you deposit ' + '<mark>' + principal + '</mark>' + ',' + '\<br\>'
+            + 'at an interest rate of ' + '<mark>' + rate + '%' + '</mark>' + ',' + '\<br\>'
+            + 'You will receive an amount of ' + '<mark>' + interest + '</mark>' + ',' + '\<br\>'
+            + 'in the year ' + '<mark>' + year + '</mark>'
+    }  */  
+   
+
+
 
 function updateRate() 
 {
@@ -28,7 +41,8 @@ function updateRate()
 }
 
 
-function checkdata(){
+/*function checkdata()
+{
     //Create references to the input elements we wish to validate    
     var principal = document.getElementById("principal").value;
      
@@ -37,14 +51,31 @@ function checkdata(){
     // var zipcode =  document.getElementById("zipcode");  
 
 //Check if principal field is empty
-if(principal <= 0){
-    alert("Enter Positive numbers or grater than Cero");
+
+ if (principal <="0")
+ {
+   //If all is well return true.
+   alert("Please enter a positive number")
+      document.getElementById("principal").focus();
+  /* return false;*/
+
+ 
+
+ /*if(principal=="")
+ {
+    alert("Number field cannot be empty");
     principal.focus();
-    return false;
+   // return false;
     }
- else 
-    //If all is well return true.
-    alert("Form validation is successful.")
-  return true;       
-  
-  } 
+
+ else if (principal<=0)
+ {
+   //If all is well return true.
+   alert("numeric field must be greated than 0")
+   principal.focus();
+   return false;
+ }
+ else{
+    //alert("continue")
+    return true;
+ }*/
